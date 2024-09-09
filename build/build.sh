a146c031ec1fcb18eb372485b8671bdc402f3a69
jact <jachavar@gmail.com>
2024-09-09 10:09:59 +0200

    feat: added pms-wpt image

--- build/build.sh
+++ build/build.sh
@@ -7,7 +7,7 @@ function build
     then
         mkdir -p $_CONTEXT/defaults/usr/bin/
         cp scripts/* $_CONTEXT/defaults/usr/bin/
-        
+
         pushd "$_CONTEXT" > /dev/null
         local _TAG=$(cat ../../VERSION)
         echo -n "${_TAG}" > ./VERSION
@@ -23,7 +23,7 @@ function build
 _IMAGES="$1"
 if [ -z "${_IMAGES}" ]
 then
-    _IMAGES="swarm proxy certbot datashare_console datastore datastore_console database database_console core console public s3 worker_console pms-apt pms-yum pms-pacman pms-winget nfs-server"
+    _IMAGES="swarm proxy certbot datashare_console datastore datastore_console database database_console core console public s3 worker_console pms-apt pms-yum pms-pacman pms-winget pms-wpt nfs-server"
 fi
 
 for _IMAGE in ${_IMAGES}
