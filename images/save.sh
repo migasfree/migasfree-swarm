14c243a795f33c30c4d0c83b69eb0eddf50552bf
jact <jachavar@gmail.com>
2024-09-09 10:09:42 +0200

    feat: added pms-wpt image

--- images/save.sh
+++ images/save.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 _VERSION=$(cat ../VERSION)
-for _IMG in swarm proxy public datashare datashare_console database database_console datastore datastore_console worker_console client certbot pms-apt pms-yum pms-winget pms-pacman
+for _IMG in swarm proxy public datashare datashare_console database database_console datastore datastore_console worker_console client certbot pms-apt pms-yum pms-winget pms-pacman pms-wpt
 do
     docker save --output ./migasfree-${_IMG}-${_VERSION}.tar migasfree/${_IMG}:${_VERSION}
 done
