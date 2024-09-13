f5d804b9f2c2dad760299e75bd58005e4e3da3f6
Alberto Gacías <alberto@migasfree.org>
2024-09-13 23:44:51 +0200

    Scripts for working with images

--- images/push.sh
+++ images/push.sh
@@ -4,7 +4,7 @@ VERSION=$(cat ../VERSION)
 # Need login
 # docker login
 
-for _IMG in swarm proxy public console datashare_console database database_console datastore datastore_console core worker_console certbot pms-apt pms-yum
+for _IMG in swarm proxy public datashare datashare_console database database_console datastore datastore_console worker_console client certbot pms-apt pms-yum pms-winget pms-pacman pms-wpt
 do
     docker push migasfree/${_IMG}:${VERSION}
 done
--- images/save.sh
+++ images/save.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
 _VERSION=$(cat ../VERSION)
-for _IMG in swarm proxy public datashare datashare_console database database_console datastore datastore_console worker_console client certbot pms-apt pms-yum pms-winget pms-pacman pms-wpt
+for _IMG in swarm proxy certbot datashare_console datastore datastore_console database database_console core console public worker_console pms-apt pms-yum pms-pacman pms-winget pms-wpt
 do
     docker save --output ./migasfree-${_IMG}-${_VERSION}.tar migasfree/${_IMG}:${_VERSION}
 done
 
-_VERSION=master
-for _IMG in console core
-do
-    docker save --output ./migasfree-${_IMG}-${_VERSION}.tar migasfree/${_IMG}:${_VERSION}
-done
-
-_VERSION=4.20
-for _IMG in client
-do
-    docker save --output ./migasfree-${_IMG}-${_VERSION}.tar migasfree/${_IMG}:${_VERSION}
-done
-
-chmod -R 644 ./*.tar
