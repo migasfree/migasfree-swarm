f05eab500a99a76bf64ab94566f0ea3576760e23
Alberto Gacías <alberto@migasfree.org>
2024-09-15 00:09:24 +0200

    Add cve.sh script

---
+++ images/cve.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# https://github.com/aquasecurity/trivy
+# apt install trivy
+
+_IMAGES="$1"
+if [ -z "${_IMAGES}" ]
+then
+    trivy clean --all
+    _IMAGES="swarm proxy certbot datashare_console datastore datastore_console database database_console core console public worker_console pms-apt pms-yum pms-pacman pms-winget pms-wpt"
+fi
+
+for _IMAGE in ${_IMAGES}
+do
+    trivy image migasfree/${_IMAGE}:$(cat ../VERSION) > ${_IMAGE}.log
+done
