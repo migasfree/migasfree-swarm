b17fc84e5f2d9d94a807a7d32b935ee3f0a19b78
Alberto Gacías <alberto@migasfree.org>
2024-09-15 19:58:16 +0200

    Add subdomains to Certbot

--- build/certbot/defaults/usr/bin/create-certificates.sh
+++ build/certbot/defaults/usr/bin/create-certificates.sh
@@ -4,7 +4,7 @@
 certbot certonly --standalone \
 --non-interactive --agree-tos --email ${EMAIL} --http-01-port=380 \
 --cert-name ${FQDN} \
--d ${FQDN}
+-d ${FQDN} -d portainer.${FQDN} -d datastore.${FQDN} -d database.${FQDN} -d datashare.${FQDN} -d worker.${FQDN} 
 
 # Concatenate certificates
 . /usr/bin/concatenate-certificates.sh
