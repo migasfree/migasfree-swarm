9acc9d2546f9d5ae4dcc755fb6bf09e35d600006
jact <jachavar@gmail.com>
2024-09-18 18:42:26 +0200

    fix: PEP8 cleanup

--- build/swarm/defaults/tools/consoles.py
+++ build/swarm/defaults/tools/consoles.py
@@ -5,32 +5,32 @@ import docker
 from context import ContextLoader, get_stacks
 
 _PATH = "/stack"  # Path in this container
-_PATH_CREDENTIALS = os.path.join(_PATH, "credentials") 
-_PATH_CERTIFICATE = os.path.join(_PATH, "certificates") 
+_PATH_CREDENTIALS = os.path.join(_PATH, "credentials")
+_PATH_CERTIFICATE = os.path.join(_PATH, "certificates")
 
-def scale_services(service_names,replicas):
 
+def scale_services(service_names, replicas):
     for service_name in service_names:
         try:
             service = client.services.get(service_name)
-            
-            current_spec = service.attrs['Spec']
-            
+
+            # current_spec = service.attrs['Spec']
+
             service.update(
                 mode={'Replicated': {'Replicas': replicas}}
             )
-            
+
             print(f'Scaled "{service_name}" to {replicas}')
-        except:
+        except Exception:
             pass
 
 # PROGRAM
 # =======
 
+
+replicas = 1
 if sys.argv[1] == "pro":
     replicas = 0
-else:
-    replicas = 1
 
 cl = ContextLoader()
 cl.load_stack(" |".join(get_stacks()))
@@ -38,12 +38,11 @@ CONTEXT = cl.context
 
 client = docker.from_env()
 
-# Ejemplo de uso
+# example of use
 consoles = [
     f"{CONTEXT['STACK']}_database_console",
     f"{CONTEXT['STACK']}_datastore_console",
     f"{CONTEXT['STACK']}_worker_console"
-    ]
+]
 
 scale_services(consoles, replicas)
-
