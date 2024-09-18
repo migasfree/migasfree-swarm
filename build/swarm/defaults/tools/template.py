a12f18105298c16b929e8afa6a5e5618abd7e584
jact <jachavar@gmail.com>
2024-09-18 18:49:33 +0200

    fix: PEP8 cleanup

--- build/swarm/defaults/tools/template.py
+++ build/swarm/defaults/tools/template.py
@@ -1,6 +1,8 @@
 import os
 import jinja2
+
 from jinja2 import Environment, FileSystemLoader
+
 
 @jinja2.pass_context
 def get_context(c):
@@ -20,4 +22,5 @@ def render(path_templates, template, context):
     template = env.get_template(template)
     template.globals['context'] = get_context
     template.globals['callable'] = callable
+
     return template.render(**context)
