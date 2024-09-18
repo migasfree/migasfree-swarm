7f240c29a1fc015df4d77d222354a3e10ba60a64
jact <jachavar@gmail.com>
2024-09-18 18:48:06 +0200

    fix: PEP8 cleanup

--- build/swarm/defaults/tools/portainer.py
+++ build/swarm/defaults/tools/portainer.py
@@ -1,8 +1,9 @@
-
 import requests
 import time
 import base64
+
 from requests.packages.urllib3.filepost import encode_multipart_formdata
+
 
 def token_exists(data, token_name):
     for element in data:
@@ -15,7 +16,6 @@ def create_token(name, username, password):
     # https://docs.portainer.io/api/access
     # https://app.swaggerhub.com/apis/portainer/portainer-ce/2.21.0#/users/UserGenerateAPIKey
 
-
     while True:
         session = None
         session = requests.Session()
@@ -24,49 +24,47 @@ def create_token(name, username, password):
 
         payload = {"username": username, "password": password}
         try:
-            response = session.post(f"http://portainer:9000/api/auth", json=payload)
-            if response and response.status_code == 200 :
+            response = session.post("http://portainer:9000/api/auth", json=payload)
+            if response and response.status_code == 200:
                 break
         except Exception as e:
             print("Error creating token:", e)
             print("****** Consider restarting the Docker service: systemctl restart docker")
         time.sleep(2)
-    
 
     if response and 'jwt' in response.json():
         headers = session.headers
         headers["Authorization"] = f"Bearer {response.json()['jwt']}"
         session.headers = headers
 
-        response = session.get(f'http://portainer:9000/api/users/me')    
+        response = session.get('http://portainer:9000/api/users/me')
         headers = session.headers
         headers["x-csrf-token"] = response.headers["x-csrf-token"]
         session.headers = headers
         if response and response.status_code == 200:
-            userid = response.json()["Id"] 
-            response = session.get(f'http://portainer:9000/api/users/{userid}/tokens')    
+            userid = response.json()["Id"]
+            response = session.get(f'http://portainer:9000/api/users/{userid}/tokens')
             if response and response.status_code == 200:
                 if not token_exists(response.json(), name):
                     # create token
-                    payload ={ "description": name, "password": password}        
+                    payload = {"description": name, "password": password}
                     response = session.post(f'http://portainer:9000/api/users/{userid}/tokens', json=payload)
                     if response and response.status_code == 201:
                         return response.json()["rawAPIKey"]
-    
-    return ""
 
+    return ""
 
 
 class PortainerAPI:
     def __init__(self, base_url, token):
         self.base_url = base_url
         self.session = requests.Session()
-        self.session.verify = False       
+        self.session.verify = False
         self.session.headers = {
             "Content-Type": "application/json",
             "X-API-Key": token
         }
-        
+
     def request(self, method, endpoint, data=None):
         url = self.base_url + endpoint
         try:
@@ -91,7 +89,7 @@ class PortainerAPI:
 
     def post(self, endpoint, data):
         return self.request("POST", endpoint, data)
-    
+
     def put(self, endpoint, data):
         return self.request("PUT", endpoint, data)
 
@@ -103,27 +101,26 @@ class PortainerAPI:
             endpoint = f"/endpoints/{self.endpoint_id}/docker/secrets/create"
             payload = {
                 "Name": name,
-                "Data": base64.b64encode(password.encode("utf-8")).decode("utf-8") 
+                "Data": base64.b64encode(password.encode("utf-8")).decode("utf-8")
             }
             response = self.post(endpoint, payload)
             if response:
                 resource_id = response['Portainer']['ResourceControl']['Id']
                 payload = {
-                    "AdministratorsOnly":True,
-                    "Public":False,
-                    "Teams":[],
-                    "Users":[]
+                    "AdministratorsOnly": True,
+                    "Public": False,
+                    "Teams": [],
+                    "Users": []
                 }
-                response = self.put(f"/resource_controls/{resource_id}", payload) 
+                response = self.put(f"/resource_controls/{resource_id}", payload)
 
     def exists_secret(self, name):
         endpoint = f"/endpoints/{self.endpoint_id}/docker/secrets"
         secrets = self.get(endpoint)
         return any(secret['Spec']['Name'] == name for secret in secrets)
-        
 
     """
-    def set_swarm_id(self, endpoint_id):    
+    def set_swarm_id(self, endpoint_id):
         response = self.get(f"/endpoints")
         for endpoint in response:
             if endpoint["Id"] == endpoint_id:
@@ -131,9 +128,9 @@ class PortainerAPI:
                 break
     """
 
-    def set_enpoint_id(self, name ):
+    def set_enpoint_id(self, name):
         while True:
-            response = self.get(f"/endpoints")
+            response = self.get("/endpoints")
             if response:
                 for endpoint in response:
                     if endpoint["Name"] == name:
@@ -143,7 +140,6 @@ class PortainerAPI:
             time.sleep(2)
 
     def create_environment(self, name):
-
         (files, content_type) = encode_multipart_formdata({
             "Name": f"{name}",
             "EndpointCreationType": "2",
@@ -154,34 +150,31 @@ class PortainerAPI:
             "TLSSkipVerify": "true",
             "TLSSkipClientVerify": "true"
         })
-        
-        headers = { "Authorization": self.session.headers["Authorization"],                    
-                    "Content-Type": content_type,
-                    "Accept": "application/json, text/plain, */*"
-                    }
+
+        headers = {
+            "Authorization": self.session.headers["Authorization"],
+            "Content-Type": content_type,
+            "Accept": "application/json, text/plain, */*"
+        }
 
         response = None
-        while response == None:
+        while response is None:
             response = self.session.post(f'{self.base_url}/endpoints', headers=headers, data=files, verify=False)
             time.sleep(1)
 
         self.set_enpoint_id(name)
 
-        #self.set_swarm_id(self.endpoint_id)
-
+        # self.set_swarm_id(self.endpoint_id)
 
     def set_public_ip(self, ip):
-        payload = { "PublicURL": ip }
-        response = self.put(f"/endpoints/{self.endpoint_id}", payload)
-        
+        payload = {"PublicURL": ip}
+        self.put(f"/endpoints/{self.endpoint_id}", payload)
 
     def custom_templates(self, payload):
-        response = self.post(f"/custom_templates?method=string", payload)
+        self.post("/custom_templates?method=string", payload)
 
-    
     def deploy(self, payload):
         self.post(f"/stacks?endpointId={self.endpoint_id}&method=string&type=1", payload)
-
 
     def settings(self):
         payload = {
@@ -191,4 +184,4 @@ class PortainerAPI:
             "SnapshotInterval": "5m",
             "TemplatesURL": "https://raw.githubusercontent.com/portainer/templates/v3/templates.json"
         }
-        self.put(f"/settings", payload)
+        self.put("/settings", payload)
