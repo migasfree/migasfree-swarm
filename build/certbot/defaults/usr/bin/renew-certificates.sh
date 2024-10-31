040a272f655b208dcfc2fd1a10de79b473a90972
Nacho Sancho <sannacho@gmail.com>
2024-10-31 18:55:49 +0100

    renew Certificate on proxy only if changed

--- build/certbot/defaults/usr/bin/renew-certificates.sh
+++ build/certbot/defaults/usr/bin/renew-certificates.sh
@@ -2,16 +2,25 @@
 
 # Certificates exist
 if [ -d /etc/letsencrypt/live/${FQDN} ]; then
+    if [ ! -f /etc/letsencrypt/live/${FQDN}/fullchain.pem.md5 ]; then
+        md5sum /etc/letsencrypt/live/${FQDN}/fullchain.pem >/etc/letsencrypt/live/${FQDN}/fullchain.pem.md5
+    fi
     # Check certificates and renew them
     certbot renew --http-01-port=380
+    if ! md5sum -c /etc/letsencrypt/live/${FQDN}/fullchain.pem.md5; then
+        md5sum /etc/letsencrypt/live/${FQDN}/fullchain.pem >/etc/letsencrypt/live/${FQDN}/fullchain.pem.md5
+        
+        echo "Certificates have been renewed"
+        
+        # Concatenate certificates
+        . /usr/bin/concatenate-certificates.sh
+        
+        echo "Renewed certificates to HAProxy"
+        # Update certificates in HAProxy
+        . /usr/bin/update-haproxy-certificates.sh
+    fi
 
-    # Concatenate certificates
-    . /usr/bin/concatenate-certificates.sh
-
-    # Update certificates in HAProxy
-    . /usr/bin/update-haproxy-certificates.sh
-
-    # Certificates don't exist
+# Certificates don't exist
 else
     #  Execute certificate creation script
     . /usr/bin/create-certificates.sh
