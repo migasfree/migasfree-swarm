9315622cf806af9397476e93e0ae6f269d87216d
Alberto Gacías <alberto@migasfree.org>
2024-12-24 12:08:53 +0100

    Improvements in image downloading

--- build/swarm/defaults/tools/pull.sh
+++ build/swarm/defaults/tools/pull.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 VERSION=$(cat /VERSION)
 
-for _IMG in proxy public console datashare_console database database_console datastore datastore_console core worker_console certbot pms-apt pms-yum
+# migasfree images
+for _IMG in swarm proxy certbot datashare_console datastore datastore_console database database_console assistant core console public worker_console pms-apt pms-yum pms-pacman pms-wpt
 do
     docker pull migasfree/${_IMG}:${VERSION}
+done
+
+# portainer images
+for _IMG in $(grep "image:" /tools/templates/portainer.template | awk  '{print $2}')
+do
+    docker pull $_IMG
 done
