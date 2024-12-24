336a7c5a27e56ad63663aa8a74789222fcb36ad1
Alberto Gacías <alberto@migasfree.org>
2024-12-24 12:18:26 +0100

    Improvements in image downloading

--- build/swarm/defaults/tools/pull.sh
+++ build/swarm/defaults/tools/pull.sh
@@ -4,11 +4,22 @@ VERSION=$(cat /VERSION)
 # migasfree images
 for _IMG in swarm proxy certbot datashare_console datastore datastore_console database database_console assistant core console public worker_console pms-apt pms-yum pms-pacman pms-wpt
 do
-    docker pull migasfree/${_IMG}:${VERSION}
+    _IMAGE="migasfree/${_IMG}:${VERSION}"
+    if docker image inspect "${_IMAGE}" > /dev/null 2>&1; then
+        echo "Image ${_IMAGE} already exists locally."
+    else
+        docker pull "${_IMAGE}"
+    fi
+
+
 done
 
 # portainer images
 for _IMG in $(grep "image:" /tools/templates/portainer.template | awk  '{print $2}')
 do
-    docker pull $_IMG
+    if docker image inspect "$_IMG" > /dev/null 2>&1; then
+        echo "Image $_IMG already exists locally."
+    else
+        docker pull "$_IMG"
+    fi
 done
