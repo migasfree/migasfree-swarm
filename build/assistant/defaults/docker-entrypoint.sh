22216fd095c578971ef5c833ff4f37f551e76e2f
Alberto Gacías <alberto@migasfree.org>
2025-09-19 19:12:04 +0200

    Complete assistant startup

--- build/assistant/defaults/docker-entrypoint.sh
+++ build/assistant/defaults/docker-entrypoint.sh
@@ -3,7 +3,31 @@
 
 cd /app/backend
 
+send_message "Starting assistant"
 init-assistant &
 
+echo "
+
+
+                   ●                          ●●
+                                             ●
+         ●●● ●●    ●    ●●     ●●●     ●●●  ●●●●  ●●●  ●●●    ●●●
+        ●   ●  ●   ●   ●  ●       ●   ●      ●   ●    ●   ●  ●   ●
+        ●   ●  ●   ●   ●  ●    ●●●●    ●●    ●   ●    ●●●●   ●●●●
+        ●   ●  ●   ●   ●  ●   ●   ●      ●   ●   ●    ●      ●
+        ●   ●  ●   ●    ●●●    ●●●    ●●●    ●   ●     ●●●    ●●●
+                          ●
+                        ●●
+
+        $SERVICE ($TAG)
+        Webui build version: $WEBUI_BUILD_VERSION
+        Container: $HOSTNAME
+        Time zome: $TZ $(date)
+        Processes: $(nproc)
+
+"
+
+
+send_message ""
 reload_proxy 30
 bash start.sh
