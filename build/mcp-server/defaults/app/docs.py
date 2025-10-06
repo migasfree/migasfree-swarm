80a6738881a7d3bc3e1856d1b6e5277a676ce7b5
jact <jachavar@gmail.com>
2025-10-06 10:01:04 +0200

    fix: PEP8 cleanup

--- build/mcp-server/defaults/app/docs.py
+++ build/mcp-server/defaults/app/docs.py
@@ -1,24 +1,21 @@
-import json
 import os
+import json
 
 from resources import read_file, download_url
-
-
+from settings import CORPUS_PATH_DOCS, RESUME_FILE_DOCS
 
 url_chapter = "https://raw.githubusercontent.com/migasfree/fun-with-migasfree/refs/heads/master/{}"
-
-from settings import CORPUS_PATH_DOCS, RESUME_FILE_DOCS
 
 
 def create_docs():
     if not os.path.exists(CORPUS_PATH_DOCS):
         os.makedirs(CORPUS_PATH_DOCS, exist_ok=True)
-        CHAPTERS =  json.loads(read_file("resources/docs/resume.json"))
+        CHAPTERS = json.loads(read_file("resources/docs/resume.json"))
         for chapter in CHAPTERS:
             filename = chapter['filename']
             download_url(url_chapter.format(filename), f"{CORPUS_PATH_DOCS}/{filename}")
 
-        with open(RESUME_FILE_DOCS,"w") as file:
+        with open(RESUME_FILE_DOCS, "w") as file:
             file.write(json.dumps(CHAPTERS))
 
 
@@ -35,6 +32,8 @@ def read_docs():
         filename = chapter['filename']
         with open(f"{CORPUS_PATH_DOCS}/{filename}", "r") as file:
             content += file.read()
+
     return content
+
 
 CONTENT_MANUAL = read_docs().replace('\n', '').replace('\r', '')
