a5520cfa5ba3efee004c5bc1e5ae27c3e479da92
jact <jachavar@gmail.com>
2025-10-06 10:02:27 +0200

    fix: PEP8 cleanup

--- build/mcp-server/defaults/app/settings.py
+++ build/mcp-server/defaults/app/settings.py
@@ -1,25 +1,25 @@
 import os
 
+from resources import read_file
+
 STACK = os.getenv("STACK", "")
 
-FQDN = os.getenv("FQDN","localhost")
+FQDN = os.getenv("FQDN", "localhost")
 
 ASSISTANT_API_URL = "http://assistant:8080"
 
-AI_PATH = f"/mnt/datashare/pool/ai"
+AI_PATH = "/mnt/datashare/pool/ai"
 
-CORPUS_PATH_DOCS = os.path.join(AI_PATH,"corpus/docs")
-RESUME_FILE_DOCS = os.path.join(CORPUS_PATH_DOCS,"resume.json")
+CORPUS_PATH_DOCS = os.path.join(AI_PATH, "corpus/docs")
+RESUME_FILE_DOCS = os.path.join(CORPUS_PATH_DOCS, "resume.json")
 
-CORPUS_PATH_DATABASE = os.path.join(AI_PATH,"corpus/database")
-RESUME_FILE_DATABASE = os.path.join(CORPUS_PATH_DATABASE,"resume.json")
+CORPUS_PATH_DATABASE = os.path.join(AI_PATH, "corpus/database")
+RESUME_FILE_DATABASE = os.path.join(CORPUS_PATH_DATABASE, "resume.json")
 
-CORPUS_PATH_API = os.path.join(AI_PATH,"corpus/api")
-RESUME_FILE_API = os.path.join(CORPUS_PATH_API,"resume.json")
+CORPUS_PATH_API = os.path.join(AI_PATH, "corpus/api")
+RESUME_FILE_API = os.path.join(CORPUS_PATH_API, "resume.json")
 
-CORPUS_PATH_EMBEDDING = os.path.join(AI_PATH,"corpus/embedding")
+CORPUS_PATH_EMBEDDING = os.path.join(AI_PATH, "corpus/embedding")
 
-with open(f'/run/secrets/{STACK}_superadmin_name', 'r', encoding='utf-8') as f:
-    SUPERADMIN_NAME = f.read()
-
+SUPERADMIN_NAME = read_file(f'/run/secrets/{STACK}_superadmin_name')
 EMAIL = f"{SUPERADMIN_NAME}@{FQDN}"
