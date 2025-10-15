74f60f2aff87be05a50ad960a7feacf7584d3e0a
Alberto Gacías <alberto@migasfree.org>
2025-10-16 00:25:28 +0200

    healthchecks

--- build/assistant/defaults/healthcheck.sh
+++ build/assistant/defaults/healthcheck.sh
@@ -2,9 +2,14 @@
 
 if curl -f http://127.0.0.1:8080
 then
-    send_message ""
+    if ! [ -f /var/tmp/healthy ]
+    then
+        touch /var/tmp/healthy
+        send_message ""
+    fi
     exit 0
 else
+    rm /var/tmp/healthy || :
     send_message "Unavailable"
     exit 1
 fi
--- build/mcp-server/defaults/healthcheck.sh
+++ build/mcp-server/defaults/healthcheck.sh
@@ -2,9 +2,14 @@
 
 if curl -f http://localhost:8080/openapi.json
 then
-    send_message ""
+    if ! [ -f /var/tmp/healthy ]
+    then
+        touch /var/tmp/healthy
+        send_message ""
+    fi
     exit 0
 else
+    rm /var/tmp/healthy || :
     send_message "Unavailable"
     exit 1
 fi
