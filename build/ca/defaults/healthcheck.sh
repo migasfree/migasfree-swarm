f91032d5007414cae134bc2ceb0598be23c9fcc2
Jose Antonio Chavarría <jachavar@gmail.com>
2025-10-16 12:25:58 +0200

    fix: health check

--- build/ca/defaults/healthcheck.sh
+++ build/ca/defaults/healthcheck.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-PROCESS_NAME="uvicorn main:app"
-
-if pgrep -f "$PROCESS_NAME" > /dev/null
+if curl -f http://127.0.0.1:80/ca/health
 then
-    HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:80/ca/health || echo "000")
-    if [ "$HTTP_CODE" == "200" ]; then
-        exit 0
-    else
-        exit 2
+    if ! [ -f /var/tmp/healthy ]
+    then
+        touch /var/tmp/healthy
     fi
+    exit 0
 else
+    rm /var/tmp/healthy || :
     exit 1
 fi
