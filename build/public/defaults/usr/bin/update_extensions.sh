fb5d6a2c72851da4c51c7986341ae467463c1b4b
jact <jachavar@gmail.com>
2025-10-24 13:26:30 +0200

    fix: removed duplicated line

--- build/public/defaults/usr/bin/update_extensions.sh
+++ build/public/defaults/usr/bin/update_extensions.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-#!/bin/sh
-
 update_haproxy_extensions() {
     proxy_host="${1:-proxy}"
     proxy_port="${2:-9999}"
     shift 2
 
-    if [ $# -eq 0 ]; then
+    if [ $# -eq 0 ]
+    then
         echo "Error: You must provide at least one extension" >&2
         return 1
     fi
@@ -15,7 +14,8 @@ update_haproxy_extensions() {
     acl_info=$(echo "show acl" | socat stdio tcp-connect:${proxy_host}:${proxy_port} | grep "virt@extensions.txt")
     acl_id=$(echo "$acl_info" | head -n1 | grep -o '^[^ ]*' | grep -o '[0-9]*')
 
-    if [ -z "$acl_id" ]; then
+    if [ -z "$acl_id" ]
+    then
         echo "Error: Could not get ACL ID for virt@extensions.txt" >&2
         return 1
     fi
@@ -24,14 +24,16 @@ update_haproxy_extensions() {
 
     new_version=$(echo "$result" | grep -o 'New version created: [0-9]*' | grep -o '[0-9]*$')
 
-    if [ -z "$new_version" ]; then
+    if [ -z "$new_version" ]
+    then
         echo "Error: Could not create a new ACL version" >&2
         return 1
     fi
 
     echo "clear acl @${new_version} #${acl_id}" | socat stdio tcp-connect:${proxy_host}:${proxy_port}
 
-    for ext in "$@"; do
+    for ext in "$@"
+    do
         clean_ext="${ext##*/}"
 
         case "$clean_ext" in
@@ -54,6 +56,7 @@ update_haproxy_extensions() {
 
 EXTENSIONS="$(curl -X GET proxy:8001/services/extensions)"
 
-for ip in $(getent hosts tasks.proxy | awk '{ print $1 }'); do
+for ip in $(getent hosts tasks.proxy | awk '{print $1}')
+do
     update_haproxy_extensions "$ip" "9999" $EXTENSIONS
 done
