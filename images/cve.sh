cf13fad54fcbaf95f5ea3c09e539e07b0697eb68
Alberto Gacías <alberto@migasfree.org>
2026-02-11 23:28:14 +0100

    cleanup: remove assistant from build process and image management

--- images/cve.sh
+++ images/cve.sh
@@ -7,7 +7,7 @@ _IMAGES="$1"
 if [ -z "${_IMAGES}" ]
 then
     trivy clean --all
-    _IMAGES="swarm proxy certbot datashare_console datastore datastore_console database database_console assistant mcp-server core console public worker_console pms-apt pms-yum pms-pacman pms-wpt"
+    _IMAGES="swarm proxy certbot datashare_console datastore datastore_console database database_console mcp-server core console public worker_console tunnel pms-apt pms-yum pms-pacman pms-wpt"
 fi
 
 for _IMAGE in ${_IMAGES}
