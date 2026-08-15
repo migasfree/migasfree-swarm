92cc7ddc32826e08fd3b9e76b3968d509d5c4daf
Alberto Gacías <alberto@migasfree.org>
2026-08-15 12:37:40 +0200

    fix(datastore_console): automate default connection in redisinsight

--- build/datastore_console/defaults/docker-entrypoint.sh
+++ build/datastore_console/defaults/docker-entrypoint.sh
@@ -12,7 +12,16 @@ wait_for_service "$REDIS_HOST" "$REDIS_PORT"
 REDISINSIGHT_VERSION=$(sed -n 's/.*"version": "\([^"]*\)".*/\1/p' /usr/src/app/redisinsight/api/dist/package.json)
 show_banner "redisinsight $REDISINSIGHT_VERSION"
 
+mkdir -p /data || :
+chown -R node:node /data || :
+
+if [ -n "$RI_APP_FOLDER_ABSOLUTE_PATH" ]; then
+    mkdir -p "$RI_APP_FOLDER_ABSOLUTE_PATH" || :
+    chown -R node:node "$RI_APP_FOLDER_ABSOLUTE_PATH" || :
+fi
+
 send_message ""
 
-su node -c "/usr/bin/add_connection" &
-su node -c "cd /usr/src/app; node redisinsight/api/dist/src/main"
+/usr/bin/add_connection &
+exec su node -c "cd /usr/src/app && node redisinsight/api/dist/src/main"
+
